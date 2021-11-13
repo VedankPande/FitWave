c0caea94ae435d06fd627dbd03331c528780aca8
Vedank Pande <vedank.pande@gmail.com>
2021-11-13 21:25:44 +0530

    fixed process function

--- DataProcessing/preprocessor.py
+++ DataProcessing/preprocessor.py
@@ -1,5 +1,7 @@
 import os
+import sys
 
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -27,20 +29,26 @@ class MovenetPreProcessor():
         output_path: Path to location where csv file should be saved
     
     '''
-    def __init__(self,input_path,model_datatype,model_type,output_path = os.getcwd()) -> None:
-        self.input_path = input_path
-        self.output_path = output_path
+    def __init__(self,input_path,model_type,model_datatype,output_path = os.getcwd()) -> None:
+        self.movenet = (model_type,model_datatype)
         self.model_type = model_type
         self.model_datatype = model_datatype
+        self.input_path = input_path
+        self.output_path = output_path
 
     @property
     def movenet(self)->tf.lite.Interpreter:
         '''
         class property that returns a tflite interpreter for the movenet model
         '''
-        model_path = os.path.join(os.getcwd(),f'movenet_{self.model_type}_{self.model_datatype}.tflite') 
+        return self.movenet_model
+
+    @movenet.setter
+    def movenet(self,model_info)->None:
+        model_type,model_datatype = model_info
+        model_path = os.path.join(os.getcwd(),'models',f'movenet_{model_type}_{model_datatype}.tflite') 
         if not os.path.exists(model_path):
-            url = f'https://tfhub.dev/google/lite-model/movenet/singlepose/{self.model_type}/tflite/{self.model_datatype}/4?lite-format=tflite'
+            url = f'https://tfhub.dev/google/lite-model/movenet/singlepose/{model_type}/tflite/{model_datatype}/4?lite-format=tflite'
             with requests.get(url) as file:
                 with open(model_path,'wb') as f:
                     try:
@@ -49,11 +57,16 @@ class MovenetPreProcessor():
                         print(e)
                         return None
             interpreter = tf.lite.Interpreter(model_path=model_path)
-            return interpreter
+            interpreter.allocate_tensors()
+            self.movenet_model = interpreter
         else:
-            return tf.lite.Interpreter(model_path=model_path)
-    
+            interpreter = tf.lite.Interpreter(model_path=model_path)
+            interpreter.allocate_tensors()
+            self.movenet_model = interpreter
+
+
     def resize_image(self,image_path)->tf.image:
+
         size = None
         if self.model_type == 'lightning':
             size = 192
@@ -66,50 +79,51 @@ class MovenetPreProcessor():
         image = tf.image.decode_jpeg(image)
         image = tf.expand_dims(image, axis=0)
         image = tf.image.resize_with_pad(image,size,size)
+        image = tf.cast(image,dtype=np.uint8)
 
         return image
     
-    def process(self):
-
+    def get_keypoints(self,image)->np.array:
+        '''
+        Returns keypoints for the given image using the class' assigned movenet variant
+        Args:
+            image: The input image on which inference is to be run
+        '''
+        pass
+    
+    def process(self)->np.array:
+        '''
+        
+        '''
+        res = []
         for exercise_class in os.listdir(self.input_path):
             class_path = os.path.join(self.input_path,exercise_class)
-            for image_name in os.listdir(class_path):
+            for image_name in tqdm(os.listdir(class_path)):
                 image_path = os.path.join(class_path,image_name)
-                if image_path.shape[-1] != 3: #not an rgb image
-                    print(f'Skipped {image_name}, not an rgb image')
-                    continue
+                # if image_path.shape[-1] != 3: #not an rgb image
+                #     print(f'Skipped {image_name}, not an rgb image')
+                #     continue
                 image = self.resize_image(image_path)
-                image = tf.cast(image,dtype=np.uint8)
-                input_details = self.movenet.get_input_details()
-                output_details = self.movenet.get_output_details()
-                self.movenet.set_tensor(input_details[0]['index'],image.numpy())
+                input_details = self.movenet_model.get_input_details()
+                output_details = self.movenet_model.get_output_details()
+                self.movenet_model.set_tensor(input_details[0]['index'],image.numpy())
 
-                self.movenet.invoke()
+                self.movenet_model.invoke()
 
-                keypoints = self.movenet.get_tensor(output_details[0]['index'])
+                keypoints = self.movenet_model.get_tensor(output_details[0]['index'])
 
-                return keypoints
+                res.append(np.squeeze(keypoints))
 
-                #TODO: save keypoints to csv (numpy->csv) or (pandas+numpy -> csv) or (numpy->pickle)
+        return res       
+        #TODO: save keypoints to csv (numpy->csv) or (pandas+numpy -> csv) or (numpy->pickle)
 
 
 
 if __name__ == "__main__":
-    image_path = 'C:\\Users\\vedan\\Desktop\\code\\PoseTrainer\\data\\pushup1_frames\\frame120.jpg'
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image)
-    image = tf.expand_dims(image, axis=0)
-    image = tf.image.resize_with_pad(image,192,192)
-    image = tf.cast(image,dtype=tf.uint8)
-    processor = MovenetPreProcessor(input_path='dummy_path',model_datatype='float16',model_type='lightning')
-    movenet = processor.movenet
-    movenet.allocate_tensors()
-    input_details = movenet.get_input_details()
-    output_details = movenet.get_output_details()
-    movenet.set_tensor(input_details[0]['index'],image.numpy())
-    movenet.invoke()
+    #env setup
+    root = 'C:\\Users\\vedan\\Desktop\\code\\PoseTrainer\\data\\frames'
 
-    keypoints = movenet.get_tensor(output_details[0]['index'])
-
-    print(keypoints)
-
+    #driver code
+    processor = MovenetPreProcessor(input_path=root,model_type='lightning',model_datatype='int8')
+    print(processor.process())
+    print(processor.movenet)
