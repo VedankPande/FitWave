e3c416f20ce2f5b5fdb5c79d97823fc480b44c75
shantanupatil23 <kanhapatildurg@gmail.com>
2022-03-18 00:18:40 +0530

    added print statements

--- workfit_app/lib/widgets/image_selector.dart
+++ workfit_app/lib/widgets/image_selector.dart
@@ -6,35 +6,37 @@ import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
 
 class ImageSelector extends StatefulWidget {
-  const ImageSelector({ Key? key }) : super(key: key);
+  const ImageSelector({Key? key}) : super(key: key);
 
   @override
   State<ImageSelector> createState() => _ImageSelectorState();
 }
 
 class _ImageSelectorState extends State<ImageSelector> {
-
   bool loading = false;
   XFile? imageFile;
   List output = [];
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    loadModel().then(
-      (value){
-        setState(() {});
-      });
+    loadModel().then((value) {
+      setState(() {});
+    });
   }
+
   @override
-  void dispose(){
+  void dispose() {
     super.dispose();
     Tflite.close();
   }
 
-  loadModel() async{
-    await Tflite.loadModel(model: 'assets/lite-model_movenet_singlepose_lightning_tflite_int8_4.tflite',
-    useGpuDelegate: false);
+  loadModel() async {
+    await Tflite.loadModel(
+        model:
+            'assets/lite-model_movenet_singlepose_lightning_tflite_int8_4.tflite',
+        useGpuDelegate: false);
+    print('model successfully loaded');
   }
 
   getPosePoints(io.File image) async {
@@ -42,26 +44,29 @@ class _ImageSelectorState extends State<ImageSelector> {
   }
 
   Future getImageFromGallery() async {
-    final XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (image==null) return;
-    
+    final XFile? image =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    print('image picked');
+    if (image == null) return;
+
+    print("image path:" + image.path);
     var model_output = await Tflite.runModelOnImage(
       path: image.path,
     );
-    log(model_output![0]);
+    print("my_data");
     setState(() {
       imageFile = image;
     });
-
-    
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        (imageFile==null)? const Text("No image selected") : Image.file(io.File(imageFile!.path)),
+        (imageFile == null)
+            ? const Text("No image selected")
+            : Image.file(io.File(imageFile!.path)),
         FloatingActionButton(onPressed: getImageFromGallery)
       ],
     );
