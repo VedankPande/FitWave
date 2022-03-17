0d1c8d52562acd59cda10d5bbae03622d7a4caaa
Vedank Pande <vedank.pande@gmail.com>
2022-03-17 23:42:37 +0530

    tflite plugin

--- workfit_app/lib/widgets/image_selector.dart
+++ workfit_app/lib/widgets/image_selector.dart
@@ -1,8 +1,9 @@
-import 'dart:ffi';
+import 'dart:developer';
 import 'dart:io' as io;
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 class ImageSelector extends StatefulWidget {
   const ImageSelector({ Key? key }) : super(key: key);
@@ -13,16 +14,48 @@ class ImageSelector extends StatefulWidget {
 
 class _ImageSelectorState extends State<ImageSelector> {
 
+  bool loading = false;
   XFile? imageFile;
+  List output = [];
+
+  @override
+  void initState(){
+    super.initState();
+    loadModel().then(
+      (value){
+        setState(() {});
+      });
+  }
+  @override
+  void dispose(){
+    super.dispose();
+    Tflite.close();
+  }
+
+  loadModel() async{
+    await Tflite.loadModel(model: 'assets/lite-model_movenet_singlepose_lightning_tflite_int8_4.tflite',
+    useGpuDelegate: false);
+  }
+
+  getPosePoints(io.File image) async {
+    var output = await Tflite.runModelOnImage(path: image.path);
+  }
 
   Future getImageFromGallery() async {
     final XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
     if (image==null) return;
     
+    var model_output = await Tflite.runModelOnImage(
+      path: image.path,
+    );
+    log(model_output![0]);
     setState(() {
       imageFile = image;
     });
+
+    
   }
+
 
   @override
   Widget build(BuildContext context) {
