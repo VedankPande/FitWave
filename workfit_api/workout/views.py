f1ddee75efb3d09a252dcfe4b0952b1083e2c475
Vedank Pande <vedank.pande@gmail.com>
2022-04-02 01:30:32 +0530

    workout app created

---
+++ workfit_api/workout/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+# Create your views here.
+
+#TODO: complete view after model schema is ready
+class WorkoutView:
+    
+    def get(self,request):
+        pass
+
+    def pose(self,request):
+        pass
