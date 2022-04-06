183fcc1e0f1e3f5fac155ef6c662e6ae6e8c46e5
Vedank Pande <vedank.pande@gmail.com>
2022-04-06 15:46:35 +0530

    created calorie app

---
+++ workfit_api/calorieTracker/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CalorietrackerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'calorieTracker'
---
+++ workfit_api/calorieTracker/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from email.policy import default
+from django.db import models
+from django.forms import DateField
+
+#TODO: Decide fields to use and finish the model
+#TODO: Test model inheritance on fooddata and foodobject
+#class FoodData(models.Model):
+#    pass
+
+#class UserDailyIntake(models.Model):
+#    date = models.DateField(default=datetime.now().date())
+# Create your models here.
