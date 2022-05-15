1540b6ba805c89b6d167b5c7b664319cb3536e78
Vedank Pande <vedank.pande@gmail.com>
2022-05-16 01:26:13 +0530

    workout post fix

--- workfit_api/workout/urls.py
+++ workfit_api/workout/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import ExerciseObjectView, WorkoutView,ExerciseDataView
 
 urlpatterns = [
+    path('workout/', WorkoutView.as_view()),
     path('workout/<owner>', WorkoutView.as_view()),
     path('workout/<int:id>', WorkoutView.as_view()),
     path('exercise-data/',ExerciseDataView.as_view()),
