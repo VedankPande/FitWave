2ac12033682d7ba5dc2df0c779edb25649374b55
shantanupatil23 <kanhapatildurg@gmail.com>
2022-06-08 00:34:45 +0530

    carousel

--- workfit_app/lib/screens/onBoarding/onBoardingRoute.dart
+++ workfit_app/lib/screens/onBoarding/onBoardingRoute.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:workfit_app/screens/onBoarding/signup/signupRoute.dart';
 import 'package:workfit_app/screens/workout/workoutPostureRoute.dart';
@@ -14,6 +15,7 @@ class OnBoarding extends StatefulWidget {
 }
 
 class _OnBoardingState extends State<OnBoarding> {
+  var _current = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,64 +24,140 @@ class _OnBoardingState extends State<OnBoarding> {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [LoginButton()],
-                  ),
-                  Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(
-                            MediaQuery.of(context).size.width * 0.1,
-                          ),
-                          child: Image.asset(
+            Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [LoginButton()],
+                      ),
+                      CarouselSlider(
+                        options: CarouselOptions(
+                          height: 400,
+                          autoPlay: true,
+                          viewportFraction: 1,
+                          onPageChanged: (index, reason) {
+                            setState(() {
+                              _current = index;
+                            });
+                          },
+                        ),
+                        items: [
+                          [
                             'assets/images/on_boarding/on_boarding_1.png',
-                          ),
-                        ),
-                        Container(
-                          width: 328,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                "ML based workout routines",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: Color(0xff232323),
-                                  fontSize: 22,
-                                  fontFamily: "Avenir",
-                                  fontWeight: FontWeight.w800,
+                            "ML based workout routines",
+                            "Workout smartly with our posture detection feature powered by Machine Learning",
+                          ],
+                          [
+                            'assets/images/on_boarding/on_boarding_2.png',
+                            "Stay fit at home",
+                            "We are here to keep you active from the comfort of your home.",
+                          ],
+                          [
+                            'assets/images/on_boarding/on_boarding_3.png',
+                            "The future of health is here",
+                            "Plan & pair your exercise routines with FitWave and lead a healthy lifestyle.",
+                          ],
+                        ].map((item) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Center(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.all(
+                                        MediaQuery.of(context).size.width * 0.1,
+                                      ),
+                                      child: Image.asset(
+                                        item[0],
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.3,
+                                      ),
+                                    ),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            item[1],
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              color: Color(0xff232323),
+                                              fontSize: 22,
+                                              fontFamily: "Avenir",
+                                              fontWeight: FontWeight.w800,
+                                            ),
+                                          ),
+                                          SizedBox(height: 10),
+                                          SizedBox(
+                                            width: 328,
+                                            child: Text(
+                                              item[2],
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                color: Color(0xff838383),
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                              SizedBox(height: 10),
-                              SizedBox(
-                                width: 328,
-                                child: Text(
-                                  "Workout smartly with our posture detection feature powered by Machine Learning",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Color(0xff838383),
-                                    fontSize: 16,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                              );
+                            },
+                          );
+                        }).toList(),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                SizedBox(height: 50),
+                Container(
+                  width: 59,
+                  height: 9,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [0, 1, 2].map((item) {
+                      return Container(
+                        width: 9,
+                        height: 9,
+                        margin: EdgeInsets.symmetric(horizontal: 4),
+                        decoration: _current == item
+                            ? BoxDecoration(
+                                borderRadius: BorderRadius.circular(32.50),
+                                gradient: LinearGradient(
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                  colors: [
+                                    Color(0xff9066ea),
+                                    Color(0xff6c4cb0)
+                                  ],
+                                ),
+                              )
+                            : BoxDecoration(
+                                borderRadius: BorderRadius.circular(32.50),
+                                color: Color(0xffd5d5d5),
+                              ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
             ),
             Container(
               width: MediaQuery.of(context).size.width,
