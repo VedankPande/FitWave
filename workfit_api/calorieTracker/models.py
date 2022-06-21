c82b8f5fdcc2d99271f1ca831c0baa70a0712dec
Vedank Pande <vedank.pande@gmail.com>
2022-06-22 01:04:50 +0530

    added meal field in foodobject

--- workfit_api/calorieTracker/models.py
+++ workfit_api/calorieTracker/models.py
@@ -1,3 +1,5 @@
+from random import choices
+from secrets import choice
 from django.utils.timezone import now
 from email.policy import default
 from time import process_time_ns
@@ -33,7 +35,19 @@ class FoodData(models.Model):
    vitamin_b12 = models.DecimalField(max_digits=7,decimal_places=2)
 
 class UserDailyIntake(models.Model):
+   BREAKFAST = 'BR'
+   LUNCH = 'LU'
+   DINNER = 'DI'
+   SNACKS = 'SN'
+   MEAL_CHOICES = [
+      (BREAKFAST,'Breakfast'),
+      (LUNCH,'Lunch'),
+      (DINNER,'Dinner'),
+      (SNACKS,'Snacks'),
+
+   ]
    user = models.CharField(max_length=255)
    date = models.DateTimeField(default=now)
    food_data = models.ForeignKey(FoodData,on_delete=models.CASCADE)
    amount = models.IntegerField()
+   meal = models.CharField(max_length=2,choices=MEAL_CHOICES,default=BREAKFAST)
