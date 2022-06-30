1d00fdf1b015dafedbac1d602635418e53bcfc7b
shantanupatil23 <kanhapatildurg@gmail.com>
2022-07-01 02:06:29 +0530

    temp ui

--- PoseReact/pose_react/src/App.js
+++ PoseReact/pose_react/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
+import "./App.css";
 
-import Posture_webcam from './posture_webcam';
-import React, { useRef } from 'react';
-import '@tensorflow/tfjs-backend-webgl';
+import Posture_webcam from "./posture_webcam";
+import React, { useRef } from "react";
+import "@tensorflow/tfjs-backend-webgl";
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  return(
+  return (
     <div>
-      <Posture_webcam/>
+      <h3>FitWave</h3>
+      <Posture_webcam />
     </div>
   );
-
 }
 
 export default App;
--- PoseReact/pose_react/src/posture_webcam.js
+++ PoseReact/pose_react/src/posture_webcam.js
@@ -1,24 +1,29 @@
-import React, { useRef } from 'react';
-import { drawKeypoints,drawSkeleton } from './draw_utils';
+import React, { useRef } from "react";
+import { drawKeypoints, drawSkeleton } from "./draw_utils";
 import * as poseDetection from "@tensorflow-models/pose-detection";
-import '@tensorflow/tfjs-backend-webgl';
+import "@tensorflow/tfjs-backend-webgl";
 import Webcam from "react-webcam";
-import {Monitor} from "./monitoring";
+import { Monitor } from "./monitoring";
 
 //TODO: Move movenet code to separate file
-export default function Posture_webcam(props){
-  var color = 'White';
-  var exercise = "Biceps";
+export default function Posture_webcam(props) {
+  var color = "White";
+  var exercise = "Press";
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER};
-  const canvas = document.getElementById('output');
+  const detectorConfig = {
+    modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER,
+  };
+  const canvas = document.getElementById("output");
   const runMovenet = async () => {
-    const movenet = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet,detectorConfig);
-    setInterval(()=> {
+    const movenet = await poseDetection.createDetector(
+      poseDetection.SupportedModels.MoveNet,
+      detectorConfig
+    );
+    setInterval(() => {
       detect(movenet);
-    },100)
-  }
+    }, 100);
+  };
 
   const detect = async (movenet) => {
     if (
@@ -37,39 +42,31 @@ export default function Posture_webcam(props){
 
       // Make Detections
       const pose = await movenet.estimatePoses(video);
-      console.log("pose points",pose);
+      console.log("pose points", pose);
       try {
-        if(Monitor(exercise,pose[0]["keypoints"])){
-          color = 'White'
-        }
-        else{
-          color = 'Red'
+        if (Monitor(exercise, pose[0]["keypoints"])) {
+          color = "White";
+        } else {
+          color = "Red";
         }
       } catch (error) {
-        console.log("angle error:",error);
+        console.log("angle error:", error);
       }
-      
 
       //TODO: Check posture and change color accordingly
 
-      drawCanvas(pose[0],video,videoWidth,videoHeight,canvasRef,color);
-
+      drawCanvas(pose[0], video, videoWidth, videoHeight, canvasRef, color);
     }
-
-    
   };
 
-  const drawCanvas = (pose,video,videoWidth,videoHeight,canvas,color) =>{
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas, color) => {
     const ctx = canvas.current.getContext("2d");
     canvas.current.width = videoWidth;
     canvas.current.height = videoHeight;
 
-    drawKeypoints(pose["keypoints"],ctx,0.5,color);
-    drawSkeleton(pose["keypoints"],ctx,0.5,color);
-    
-
-
-  }
+    drawKeypoints(pose["keypoints"], ctx, 0.5, color);
+    drawSkeleton(pose["keypoints"], ctx, 0.5, color);
+  };
 
   runMovenet();
 
@@ -86,38 +83,36 @@ export default function Posture_webcam(props){
     <div className="App">
       <header className="App-header">
         <Webcam
-        audio = {false}
-        ref = {webcamRef}
-        style = {{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zIndex: 9,
-          width: 1920,
-          height: 1080,
-        }}>
-        </Webcam>
+          audio={false}
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 1280,
+            height: 720,
+          }}
+        ></Webcam>
 
         <canvas
-        ref = {canvasRef}
-        style = {{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zIndex: 9,
-          width: 1920,
-          height: 1080,
-        }}>
-
-        </canvas>
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 1080,
+            height: 720,
+          }}
+        ></canvas>
       </header>
     </div>
   );
 }
-
