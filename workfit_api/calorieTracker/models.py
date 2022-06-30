6390e71ebb350e0cd27dc0c005fbf460701eeb73
Vedank Pande <vedank.pande@gmail.com>
2022-07-01 00:01:33 +0530

    minor changes

--- workfit_api/calorieTracker/models.py
+++ workfit_api/calorieTracker/models.py
@@ -16,23 +16,7 @@ class FoodData(models.Model):
    fat = models.DecimalField(max_digits=7,decimal_places=2)
    carbohydrate = models.DecimalField(max_digits=7,decimal_places=2) 
    calorie = models.DecimalField(max_digits=7,decimal_places=2)
-   water = models.DecimalField(max_digits=7,decimal_places=2) 
-   sugar = models.DecimalField(max_digits=7,decimal_places=2) 
-   fiber = models.DecimalField(max_digits=7,decimal_places=2) 
-   calcium = models.DecimalField(max_digits=7,decimal_places=2)
-   iron = models.DecimalField(max_digits=7,decimal_places=2) 
-   magnesium = models.DecimalField(max_digits=7,decimal_places=2) 
-   phosphorus = models.DecimalField(max_digits=7,decimal_places=2)
-   potassium = models.DecimalField(max_digits=7,decimal_places=2) 
-   sodium = models.DecimalField(max_digits=7,decimal_places=2) 
-   zinc = models.DecimalField(max_digits=7,decimal_places=2) 
-   copper = models.DecimalField(max_digits=7,decimal_places=2) 
-   vitamin_a = models.DecimalField(max_digits=7,decimal_places=2) 
-   vitamin_e = models.DecimalField(max_digits=7,decimal_places=2) 
-   vitamin_d = models.DecimalField(max_digits=7,decimal_places=2) 
-   vitamin_c = models.DecimalField(max_digits=7,decimal_places=2)
-   vitamin_b6 = models.DecimalField(max_digits=7,decimal_places=2)
-   vitamin_b12 = models.DecimalField(max_digits=7,decimal_places=2)
+
 
 class UserDailyIntake(models.Model):
    BREAKFAST = 'BR'
