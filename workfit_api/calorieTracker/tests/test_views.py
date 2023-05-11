cc06c2b82cb057e1cdbdb03d9a341e5ef520517d
shantanupatil23 <kanhapatildurg@gmail.com>
2023-05-11 16:59:56 +0530

    setup test

---
+++ workfit_api/calorieTracker/tests/test_views.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+class PublicApiTest(TestCase):
+    """Test the public features of the user API."""
+
+    def test_public_api(self):
+        """Test public API."""
+        self.assertEqual(0,0)
